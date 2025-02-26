3d46fc825afe7f371e42ece0083a091fdc2b64bb
narayan <murgod@google.com>
2025-02-26 19:57:30 +0000

    added auth

--- main.tf
+++ main.tf
@@ -11,6 +11,31 @@ provider "google" {
   region  = var.region
 }
 
+locals {
+ terraform_service_account = "terraform@cts05-murgod.iam.gserviceaccount.com"
+}
+
+provider "google" {
+ alias = "impersonation"
+ scopes = [
+   "https://www.googleapis.com/auth/cloud-platform",
+   "https://www.googleapis.com/auth/userinfo.email",
+ ]
+}
+
+data "google_service_account_access_token" "default" {
+ provider               	= google.impersonation
+ target_service_account 	= local.terraform_service_account
+ scopes                 	= ["userinfo-email", "cloud-platform"]
+ lifetime               	= "1200s"
+}
+
+provider "google" {
+ project 		= "cts05-murgod"
+ access_token	= data.google_service_account_access_token.default.access_token
+ request_timeout 	= "60s"
+}
+
 # VPC Network Configuration
 module "vpc" {
   source  = "terraform-google-modules/network/google"
